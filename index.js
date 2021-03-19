917d1becc844701b762d588135060d8e2c234b6c
putrialisyahm <putrialisyahmariska@gmail.com>
2021-03-19 13:43:27 +0700

    finalproject

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ const cors = require('cors');
 
 const app = express(); // Make API
 app.use(cors());
-const key = fs.readFileSync("./ssl/privkey.pem", "utf-8");
-const cert = fs.readFileSync("./ssl/fullchain.pem", "utf-8");
+// const key = fs.readFileSync("./ssl/privkey.pem", "utf-8");
+// const cert = fs.readFileSync("./ssl/fullchain.pem", "utf-8");
 
 //Set body parser for HTTP post operation
 app.use(cors());
@@ -33,5 +33,5 @@ app.listen(3000, () => {
   console.log('User running on port 3000!');
 })
 
-https.createServer({ key: key, cert: cert }, app).listen(3002); // Define app
+// https.createServer({ key: key, cert: cert }, app).listen(3002); // Define app
 module.exports = app;
